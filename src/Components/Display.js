6400e894c821448c26c6947f7dd25e76fad9d3d2
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-09-30 04:43:14 +0530

    Display time with format

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const Display = () => {
+const Display = (props) => {
     return (
-        <div>Display</div>
+        <div>
+            <span>{(props.time.h >= 10) ? props.time.h : "0" + props.time.h}</span>&nbsp;:&nbsp;
+            <span>{(props.time.m >= 10) ? props.time.m : "0" + props.time.m}</span>&nbsp;:&nbsp;
+            <span>{(props.time.s >= 10) ? props.time.s : "0" + props.time.s}</span>&nbsp;:&nbsp;
+            <span>{(props.time.ms >= 10) ? props.time.ms : "0" + props.time.ms}</span>
+        </div>
     )
 }
 

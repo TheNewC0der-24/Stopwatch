c822870fb4544614c174347e3ec3b2b2870ee4b7
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-09-30 04:41:24 +0530

    🦄 refactor: Create state for Hour, Minute, Second and Millisecond

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import './App.css';
+import React, { useState } from 'react';
 import Display from './Components/Display';
 import Btn from './Components/Btn';
+import './App.css';
 
 function App() {
+
+  const [time, setTime] = useState({ ms: 0, s: 0, m: 0, h: 0 });
+
   return (
     <>
       <div className='main-section'>
         <div className="clock-holder">
           <div className="stopwatch">
-            <Display />
+            <Display time={time} />
             <Btn />
           </div>
         </div>

6f069071fad66cc51f84eca1fa4349ae5f3be260
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-10-03 02:37:23 +0530

    ✨ feat: For start stopwatch
    Create the run function to start the stopwatch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,30 @@ function App() {
   const [time, setTime] = useState({ ms: 0, s: 0, m: 0, h: 0 });
 
   const start = () => {
+    run();
+    setInterval(run, 10);
+  }
 
+  var updateMs = time.ms,
+    updateS = time.s,
+    updateM = time.m,
+    updateH = time.h;
+
+  const run = () => {
+    if (updateM === 60) {
+      updateH++;
+      updateM = 0;
+    }
+    if (updateS === 60) {
+      updateM++;
+      updateS = 0;
+    }
+    if (updateMs === 100) {
+      updateS++;
+      updateMs = 0;
+    }
+    updateMs++;
+    return setTime({ ms: updateMs, s: updateS, m: updateM, h: updateH });
   }
 
   return (

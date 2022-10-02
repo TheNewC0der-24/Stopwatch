34f3a0f5cd4367dfb39d14a6d7079139de7ef885
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-10-03 02:51:21 +0530

    Set Status to the buttons

--- src/Components/Btn.js
+++ src/Components/Btn.js
@@ -3,12 +3,52 @@ import React from 'react';
 const Btn = (props) => {
     return (
         <div>
-            <button
-                className='stopwatch-btn stopwatch-btn-gre'
-                onClick={props.start}
-            >
-                Start
-            </button>
+            {
+                (props.status === 0) ?
+                    <button
+                        className='stopwatch-btn stopwatch-btn-gre'
+                        onClick={props.start}
+                    >
+                        Start
+                    </button> : ""
+            }
+
+            {
+                (props.status === 1) ?
+                    <div>
+                        <button
+                            className='stopwatch-btn stopwatch-btn-red'
+                            onClick={props.stop}
+                        >
+                            Stop
+                        </button>
+                        <button
+                            className='stopwatch-btn stopwatch-btn-yel'
+                            onClick={props.reset}
+                        >
+                            Reset
+                        </button>
+                    </div> : ""
+            }
+
+            {
+                (props.status === 2) ?
+                    <div>
+                        <button
+                            className='stopwatch-btn stopwatch-btn-gre'
+                            onClick={props.resume}
+                        >
+                            Resume
+                        </button>
+                        <button
+                            className='stopwatch-btn stopwatch-btn-yel'
+                            onClick={props.reset}
+                        >
+                            Reset
+                        </button>
+                    </div> : ""
+            }
+
         </div>
     )
 }
